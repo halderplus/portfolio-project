c7a98ebfc98889a67889e2959f1b3f2a8a3e3c43
halderplus@gmail.com <halderplus@gmail.com>
2020-02-22 05:53:30 -0800

    Blog and Jobs final

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -96,6 +96,13 @@ USE_L10N = True
 USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'jobs/store'),
+    os.path.join(BASE_DIR, 'portfolio/static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
